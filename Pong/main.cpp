eed6b473f0266bb92a156fbc1b189a541e2ae459
laptop_unmaged <unknown6458758@gmail.com>
2026-02-13 16:21:23 +0100

    Improved Codestyle and logics

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -4,11 +4,11 @@
 //farbiger score für looser und winner
 //menu machen + main eigene methode
 void shuffle(float &ballMoveX, float &ballMoveY, std::mt19937 &gen) {
-	std::uniform_real_distribution<float>dist(-5.0f, 5.0f);
+	std::uniform_real_distribution<float>dist(-600.0f, 600.0f);
 	do {
 		ballMoveX = dist(gen);
 		ballMoveY = dist(gen);
-	} while (ballMoveX < 1 && ballMoveX > -1 || ballMoveY < 1 && ballMoveY > -1.0f);
+	} while (ballMoveX < 120.0f && ballMoveX > -120.0f || ballMoveY < 120.0f && ballMoveY > -120.0f);
 	std::cout << "function shuffle called" << std::endl;
 	std::cout << "speed X: " << ballMoveX << " Speed Y: " << ballMoveY << std::endl;
 }
@@ -16,18 +16,17 @@ void shuffle(float &ballMoveX, float &ballMoveY, std::mt19937 &gen) {
 int main() {
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	std::uniform_real_distribution<float>dist(-5.0f, 5.0f);
 	//Variables
-	float playerspeed = 3.0f;
-	float ballMoveIncrement = 0.5f;
+	float playerspeed = 360;
+	float ballMoveIncrement = 60.0f;
 	int score1 = 0;
 	int score2 = 0;
 
-	float ballMoveX;
-	float ballMoveY;
+	float ballMoveX = 0.0f;
+	float ballMoveY = 0.0f;
 	shuffle(ballMoveX, ballMoveY, gen);
 
-	
+	sf::Clock clock;
 
 	//window
 	unsigned int width = 1920;
@@ -36,69 +35,66 @@ int main() {
 	window->setFramerateLimit(120);
 	
 	//frame / border
-	sf::RectangleShape borderUp({ 1920.0f, 5.0f });
-	borderUp.setOrigin({ 960.0f, 2.5f });
-	borderUp.setPosition({ 960.0f, 2.5f });
+	sf::RectangleShape borderUp({ (float)width, 5.0f });
+	borderUp.setPosition({ width / 2.0f, 2.5f });
+	borderUp.setOrigin({ width / 2.0f, 2.5f });
 
-	sf::RectangleShape borderDown({ 1920.0f, 5.0f });
-	borderDown.setPosition({ 960.0f, 1077.5 });
-	borderDown.setOrigin({ 960.0f, 2.5f });
+	sf::RectangleShape borderDown({ (float)width, 5.0f });
+	borderDown.setPosition({ width / 2.0f, height - 2.5f });
+	borderDown.setOrigin({ width / 2.0f, 2.5f });
 
 	//player1
 	sf::RectangleShape player1;
 	player1.setSize({20.0f, 270.0f});
 	player1.setOrigin({10.0f, 135.0f});
-	player1.setPosition({ 40.0f, 540.0f });
+	player1.setPosition({ 40.0f, height / 2.0f });
 
 	//player2
 	sf::RectangleShape player2;
 	player2.setSize({ 20.0f, 270.0f });
 	player2.setOrigin({ 10.0f, 135.0f });
-	player2.setPosition({ 1880.0f, 540.0f });
+	player2.setPosition({ width - 40.0f, height / 2.0f });
 
 	//goals
 	sf::RectangleShape goalPlayer1;
-	goalPlayer1.setSize({ 1.0f, 1080.0f });
-	goalPlayer1.setOrigin({ 0.5f, 540.0f });
-	goalPlayer1.setPosition({ -1.0f, 540.0f });
+	goalPlayer1.setSize({ 1.0f, (float)height});
+	goalPlayer1.setOrigin({ 0.5f, height / 2.0f });
+	goalPlayer1.setPosition({ 0.5f, height / 2.0f });
 
 	sf::RectangleShape goalPlayer2;
-	goalPlayer2.setSize({ 1.0f, 1080.0f });
-	goalPlayer2.setOrigin({ 0.5f, 540.0f });
-	goalPlayer2.setPosition({ 1921.0f, 540.0f });
+	goalPlayer2.setSize({ 1.0f, (float)height});
+	goalPlayer2.setOrigin({ 0.5f, height / 2.0f });
+	goalPlayer2.setPosition({ width - 0.5f, height / 2.0f });
 
 	//ball
 	sf::CircleShape ball(20.0f);
 	ball.setOrigin(ball.getGeometricCenter());
 	ball.setFillColor(sf::Color::Magenta);
-	ball.setPosition({ 960.0f, 540.0f });
-	ball.setPointCount(50);
+	ball.setPosition({ width / 2.0f, height / 2.0f });
+	ball.setPointCount(30);
 
 	//Assets
 	sf::Font font;
-	//For Release
-	/*if (!font.openFromFile("../x64/Release/assets/fonts/myFont.ttf")) {
-		std::cerr << "Font couldnt get loaded!" << std::endl;
-	} */
-	
-	//For own
+
 	if (!font.openFromFile("assets/fonts/myFont.ttf")) {
-		std::cerr << "Font couldnt get loaded!" << std::endl;
+		std::cerr << "Font couldn't get loaded!\n";
 	}
 	
 	//Scores
+	sf::FloatRect textRect;
 	sf::Text scorePlayer1(font);
 	scorePlayer1.setString(std::to_string(score1));
 	scorePlayer1.setCharacterSize(30);
-	sf::FloatRect textRect = scorePlayer1.getLocalBounds();
+	textRect = scorePlayer1.getLocalBounds();
 	scorePlayer1.setOrigin(textRect.getCenter());
 	scorePlayer1.setPosition({30.0f, 30.0f});
 
 	sf::Text scorePlayer2(font);
 	scorePlayer2.setString(std::to_string(score2));
 	scorePlayer2.setCharacterSize(30);
+	textRect = scorePlayer2.getLocalBounds();
 	scorePlayer2.setOrigin(textRect.getCenter());
-	scorePlayer2.setPosition({ 1890.0f, 30.0f });
+	scorePlayer2.setPosition({ width - 30.0f, 30.0f });
 
 	//Game
 	while (window->isOpen()) {
@@ -112,61 +108,69 @@ int main() {
 				}
 			}
 		}
+		float dt = clock.restart().asSeconds();
 
-		ball.move({ ballMoveX, ballMoveY });
+		ball.move({ ballMoveX * dt, ballMoveY * dt});
+
+		auto p1Bounds = player1.getGlobalBounds();
+		auto p2Bounds = player2.getGlobalBounds();
+		auto upBounds = borderUp.getGlobalBounds();
+		auto downBounds = borderDown.getGlobalBounds();
+		float topLimit = borderUp.getPosition().y + borderUp.getSize().y;
+		float bottomLimit = borderDown.getPosition().y;
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::W)) {
-			if (!player1.getGlobalBounds().findIntersection(borderUp.getGlobalBounds())) {
-				player1.move({ 0.0f, -playerspeed });
+			if (p1Bounds.position.y > topLimit) {
+				player1.move({ 0.0f, -playerspeed * dt});
 			}
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::S)) {
-			if (!player1.getGlobalBounds().findIntersection(borderDown.getGlobalBounds())) {
-				player1.move({ 0.0f, playerspeed });
+			if (p1Bounds.position.y + p1Bounds.size.y < bottomLimit) {
+				player1.move({ 0.0f, playerspeed * dt});
 			}
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Up)) {
-			if (!player2.getGlobalBounds().findIntersection(borderUp.getGlobalBounds())) {
-				player2.move({ 0.0f, -playerspeed });
+			if (p2Bounds.position.y > topLimit) {
+				player2.move({ 0.0f, -playerspeed * dt});
 			}
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Down)) {
-			if (!player2.getGlobalBounds().findIntersection(borderDown.getGlobalBounds())) {
-				player2.move({ 0.0f, playerspeed });
+			if (p2Bounds.position.y + p2Bounds.size.y < bottomLimit) {
+				player2.move({ 0.0f, playerspeed * dt});
 			}
 		}
-
-		if (ball.getGlobalBounds().findIntersection(borderUp.getGlobalBounds())) { // - -> +
-			ballMoveY = -ballMoveY;
-			ballMoveY += ballMoveIncrement;
+		auto ballBounds = ball.getGlobalBounds();
+		if (ballBounds.findIntersection(upBounds)&& ballMoveY < 0) { // - -> +
+			ballMoveY = -ballMoveY + ballMoveIncrement;
 		}
-		if (ball.getGlobalBounds().findIntersection(borderDown.getGlobalBounds())) { // + -> -
-			ballMoveY = -ballMoveY;
-			ballMoveY -= ballMoveIncrement;
+		if (ballBounds.findIntersection(downBounds)&& ballMoveY > 0) { // + -> -
+			ballMoveY = -ballMoveY - ballMoveIncrement;
 		}
-		if (ball.getGlobalBounds().findIntersection(player1.getGlobalBounds())) { // - -> +
-			ballMoveX = -ballMoveX;
-			ballMoveX += ballMoveIncrement;
+		if (ballBounds.findIntersection(p1Bounds)&& ballMoveX < 0) { // - -> +
+			ballMoveX = -ballMoveX + ballMoveIncrement;
 		}
-		if (ball.getGlobalBounds().findIntersection(player2.getGlobalBounds())) { // + -> -
-			ballMoveX = -ballMoveX;
-			ballMoveX -= ballMoveIncrement;
+		if (ballBounds.findIntersection(p2Bounds)&& ballMoveX > 0) { // + -> -
+			ballMoveX = -ballMoveX - ballMoveIncrement;
 		}
-		if (ball.getGlobalBounds().findIntersection(goalPlayer1.getGlobalBounds())) {
-			ball.setPosition({ 960.0f, 540.0f });
+		bool goal = false;
+		if (ballBounds.findIntersection(goalPlayer1.getGlobalBounds())) {
 			score2++;
+			goal = true;
 			scorePlayer2.setString(std::to_string(score2));
-			shuffle(ballMoveX, ballMoveY, gen);
-			player1.setPosition({ 40.0f, 540.0f });
-			player2.setPosition({ 1880.0f, 540.0f });
+			scorePlayer2.setOrigin(scorePlayer2.getLocalBounds().getCenter());
 		}
-		if (ball.getGlobalBounds().findIntersection(goalPlayer2.getGlobalBounds())) {
-			ball.setPosition({ 960.0f, 540.0f });
+		else if (ballBounds.findIntersection(goalPlayer2.getGlobalBounds())) {
 			score1++;
+			goal = true;
 			scorePlayer1.setString(std::to_string(score1));
-			shuffle(ballMoveX, ballMoveY, gen);
+			scorePlayer1.setOrigin(scorePlayer1.getLocalBounds().getCenter());
+
+		}
+		if (goal) {
 			player1.setPosition({ 40.0f, 540.0f });
 			player2.setPosition({ 1880.0f, 540.0f });
+			ball.setPosition({ 960.0f, 540.0f });
+			shuffle(ballMoveX, ballMoveY, gen);
 		}
 
 		window->clear(sf::Color::Black);
